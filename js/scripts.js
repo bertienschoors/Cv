2d10ced7d950f9ff606d95c0b8731245fbb2ec6d
bertienschoors <bertienschoors@hotmail.com>
2021-05-07 15:13:18 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+
+(function ($) {
+    "use strict";
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                anime({
+                    targets: 'html, body',
+                    scrollTop: target.offset().top,
+                    duration: 1000,
+                    easing: 'easeInOutExpo'
+                });
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").on('click', function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav"
+    });
+
+    jQuery(document).ready(function() {
+        jQuery('.skillbar').each(function() {
+          jQuery(this).find('.skillbar-bar').animate({
+            width: jQuery(this).attr('data-percent')
+          }, 4000);
+        });
+      });
+      
+})(jQuery); // End of use strict
